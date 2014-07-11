3f3fa3750abef8983c2905efb82f603d9ec2ec28
Duany Dreyton Bezerra Sousa <duanyrf@gmail.com>
2014-07-11 10:51:54 -0300

    Adicionar funcionalidade de leitura de arquivos texto com instancias

--- Programa.java
+++ Programa.java
@@ -1,17 +1,47 @@
 package teste;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 
 
 // S carregar
 public class Programa {
+	
+	private static String[][] vetor;
 
-	public static void main(String[] args) throws CloneNotSupportedException {
+	public static void main(String[] args) throws CloneNotSupportedException, FileNotFoundException {
 
-		Filogenia f = new Filogenia();
-		f.load(8,12);
-		Algoritmo algoritmo = new Algoritmo();
-		algoritmo.execute(8, 12, 20);
+//		Filogenia f = new Filogenia();
+//		f.load(8,12);
+//		Algoritmo algoritmo = new Algoritmo();
+//		algoritmo.execute(8, 12, 20);
 		
+		vetor = new String[8][5];
+		
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(new FileReader("src/teste/arquivo.txt")).useDelimiter("\\n");
+		for (int i = 0; i < 8; i++) {
+			while (scanner.hasNext()) {
+				String nota1 = scanner.next();
+				String nota2 = scanner.next();
+				String nota3 = scanner.next();
+				String nota4 = scanner.next();
+				vetor[i][0] = String.valueOf(i + 1);
+				vetor[i][1] = nota1;
+				vetor[i][2] = nota2;
+				vetor[i][3] = nota3;
+				vetor[i][4] = nota4;
+			}
+		}
+		
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 5; j++) {
+				System.out.print(vetor[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 
 }
